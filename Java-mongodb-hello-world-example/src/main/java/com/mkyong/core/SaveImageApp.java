25c5924ac275d45b9ac09188fff5467ea5f0b9d7
wongkeaw <oa.wongkeaw@gmail.com>
2018-10-17 02:06:45 +0700

    Initial commit

---
+++ Java-mongodb-hello-world-example/src/main/java/com/mkyong/core/SaveImageApp.java
@@ -0,0 +1,71 @@
+package com.mkyong.core;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+import com.mongodb.gridfs.GridFS;
+import com.mongodb.gridfs.GridFSDBFile;
+import com.mongodb.gridfs.GridFSInputFile;
+
+/**
+ * Java MongoDB : Save image example
+ * 
+ */
+
+public class SaveImageApp {
+	public static void main(String[] args) {
+
+		try {
+
+			Mongo mongo = new Mongo("localhost", 27017);
+			DB db = mongo.getDB("imagedb");
+			DBCollection collection = db.getCollection("dummyColl");
+
+			String newFileName = "mkyong-java-image";
+
+			File imageFile = new File("C:\\Users\\ongOngongPC\\Pictures\\JavaWebHosting.png");
+
+			// create a "photo" namespace
+			GridFS gfsPhoto = new GridFS(db, "photo");
+
+			// get image file from local drive
+			GridFSInputFile gfsFile = gfsPhoto.createFile(imageFile);
+
+			// set a new filename for identify purpose
+			gfsFile.setFilename(newFileName);
+
+			// save the image file into mongoDB
+			gfsFile.save();
+
+			// print the result
+			DBCursor cursor = gfsPhoto.getFileList();
+			while (cursor.hasNext()) {
+				System.out.println(cursor.next());
+			}
+
+			// get image file by it's filename
+			GridFSDBFile imageForOutput = gfsPhoto.findOne(newFileName);
+
+			// save it into a new image file
+			imageForOutput.writeTo("C:\\Users\\ongOngongPC\\Pictures\\JavaWebHostingNew.png");
+
+			// remove the image file from mongoDB
+			gfsPhoto.remove(gfsPhoto.findOne(newFileName));
+
+			System.out.println("Done");
+
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (MongoException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
